5a79ce359c28ad84f69b0ede3aa870f12cb53bb6
Sharath.Dharmala <sharath.dharmala@gmail.com>
2025-04-02 22:33:40 +0200

    db congurations adjustments

--- user-management/src/main/java/com/riseroots/usermanagement/UserManagementApplication.java
+++ user-management/src/main/java/com/riseroots/usermanagement/UserManagementApplication.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.riseroots.usermanagement")
 @EntityScan(basePackages = "com.riseroots.usermanagement.model")
 @EnableJpaRepositories(basePackages = "com.riseroots.usermanagement.repository")
